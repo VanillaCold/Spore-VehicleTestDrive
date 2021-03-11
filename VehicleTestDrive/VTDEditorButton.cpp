58c9203da9da23358f20a800390d470e86062c79
VanillaCold <VanillaCold@users.noreply.github.com>
2021-03-11 19:47:34 +0000

    Add button to load into test drive through editor

---
+++ VehicleTestDrive/VTDEditorButton.cpp
@@ -0,0 +1,96 @@
+#include "stdafx.h"
+#include "VTDEditorButton.h"
+#include "MyGameMode.h"
+#include <Spore/Editors/BakeManager.h>
+
+VTDEditorButton::VTDEditorButton()
+{
+}
+
+
+VTDEditorButton::~VTDEditorButton()
+{
+}
+
+// For internal use, do not modify.
+int VTDEditorButton::AddRef()
+{
+	return DefaultRefCounted::AddRef();
+}
+
+// For internal use, do not modify.
+int VTDEditorButton::Release()
+{
+	return DefaultRefCounted::Release();
+}
+
+// You can extend this function to return any other types your class implements.
+void* VTDEditorButton::Cast(uint32_t type) const
+{
+	CLASS_CAST(Object);
+	CLASS_CAST(IWinProc);
+	CLASS_CAST(VTDEditorButton);
+	return nullptr;
+}
+
+// This method returns a combinations of values in UTFWin::EventFlags.
+// The combination determines what types of events (messages) this window procedure receives.
+// By default, it receives mouse/keyboard input and advanced messages.
+int VTDEditorButton::GetEventFlags() const
+{
+	return kEventFlagBasicInput | kEventFlagAdvanced;
+}
+
+// The method that receives the message. The first thing you should do is probably
+// checking what kind of message was sent...
+bool VTDEditorButton::HandleUIMessage(IWindow* window, const Message& message)
+{
+	if (auto savebutton = WindowManager.GetMainWindow()->FindWindowByID(0x05B6E484)) {
+		if (savebutton->IsEnabled()) { window->SetShadeColor(Math::Color(128, 128, 128, 255));  return false; }
+		else { window->SetShadeColor(Math::Color(255, 255, 255, 255)); }
+	}
+	else { window->SetShadeColor(Math::Color(255, 255, 255, 255)); }
+	if (message.IsType(MessageType::kMsgButtonClick) || message.IsType(MessageType::kMsgButtonSelect))
+	{
+		auto selection = Editor.mpEditorModel->field_0C;
+		if (selection != ResourceKey(0, 0, 0)) //check if creation was even selected or not
+		{
+			MyGameMode::prevGameMode = GameModeManager.GetActiveModeID();
+			//MyGameMode::
+			MyGameMode::selection = selection;
+
+			PropertyListPtr propList;
+			if (!PropManager.GetPropertyList(selection.instanceID, selection.groupID, propList))
+			{
+				MyGameMode::editor = new Editors::EditorRequest();
+				auto test = Editors::GetEditor();
+				test->editorTranslateModelOnSave = test->editorTranslateModelOnSave;
+				if (GameModeManager.GetActiveModeID() == kEditorMode)
+				{
+					MyGameMode::editor = Editor.mEditorRequest;
+					if (Editors::GetEditor()->mpEditorModel != nullptr) { MyGameMode::editor->creationKey = Editors::GetEditor()->mpEditorModel->field_0C; }
+				}
+				else { MyGameMode::editor = nullptr; }
+				BakeManager.func4Ch(selection, NULL); //bake model
+				GameModeManager.SetActiveMode(id("VehicleTestDriveGM")); //set game mode
+				return true;
+			}
+			else
+			{
+				auto test = Editors::GetEditor();
+				//test->editorTranslateModelOnSave = test->editorTranslateModelOnSave;
+				if (GameModeManager.GetActiveModeID() == kEditorMode)
+				{
+					MyGameMode::editor = Editor.mEditorRequest;
+					if (Editors::GetEditor()->mpEditorModel != nullptr) { MyGameMode::editor->creationKey = Editors::GetEditor()->mpEditorModel->field_0C; }
+				}
+				else { MyGameMode::editor = nullptr; }
+				//BakeManager.func4Ch(selection, NULL); //bake model
+				GameModeManager.SetActiveMode(id("VehicleTestDriveGM")); //set game mode
+				return true;
+			}
+		}
+	}
+	// Return true if the message was handled, and therefore no other window procedure should receive it.
+	return false;
+}
--- VehicleTestDrive/dllmain.cpp
+++ VehicleTestDrive/dllmain.cpp
@@ -3,6 +3,7 @@
 #include "MyCheat.h"
 #include "MyGameMode.h"
 #include "VTDCamera.h"
+#include "VTDEditorButton.h"
 
 App::ICamera* VTDCameraFactory(App::PropertyList* propList)
 {
@@ -32,8 +33,41 @@ void Dispose()
 	// This method is called when the game is closing
 }
 
+member_detour(VTDEditorDetour, Editors::cEditor, bool())
+{
+	bool detoured()
+	{
+		auto origfun = original_function(this); //run the original function first, so that the UI will actually exist.
+		auto window = WindowManager.GetMainWindow()->FindWindowByID(0x066FF240); //find window that normally obstructs test drive button in non-creature editors
+		if (window != nullptr)
+		{
+			SporeDebugPrint("The detour is working!");
+			bool isskin = 0;
+			App::Property::GetBool(mpPropList.get(), 0x300DE90B, isskin); //check if editor uses skin.
+			if (isskin == 0) //if editor uses skin, just skip all of the following code because skin-based editors work fine for the normal test drive
+			{
+				auto window2 = window->FindWindowByID(0);
+				if (window2 != nullptr) //check if 'Test Drive in All Editors' mod is installed
+				{
+					window2->AddWinProc(new VTDEditorButton); //if so, just use the button added by that mod instead of making a new button.
+					return origfun;
+				}
+				auto layout = new UTFWin::UILayout;
+				layout->LoadByName(u"vtd_button"); //load in vtd_button spui
+				layout->SetParentWindow(window); //set parent window for button
+				auto window3 = window->FindWindowByID(id("vtd_editorbutton")); //find button through control ID
+				window3->SetArea(Math::Rectangle(10, 10, 56, 56)); //set position of button
+
+				if (window3 != nullptr) window3->AddWinProc(new VTDEditorButton); //set win proc for button
+			}
+		}
+		return origfun;
+	}
+};
+
 void AttachDetours()
 {
+	VTDEditorDetour::attach(GetAddress(Editors::cEditor, OnEnter));
 	// Call the attach() method on any detours you want to add
 	// For example: cViewer_SetRenderType_detour::attach(GetAddress(cViewer, SetRenderType));
 }
